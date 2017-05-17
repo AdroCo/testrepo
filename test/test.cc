f940e13fe5d395bb395d32c4e9fa506644c21452
Kamran Amini <kam.cpp@gmail.com>
2017-05-17 14:40:00 +0200

    Test are now failing.

--- test/test.cc
+++ test/test.cc
@@ -3,9 +3,14 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+  bool test_res = true;
   int res = add(3, 4);
-  if (res == 7) {
-    return 0;
+  if (res != 7) {
+    test_res = false;
   }
-  return 1;
+  int res2 = add(4, 5);
+  if (res2 != 7) {
+    test_res = false;
+  }
+  return test_res ? 0 : 1;
 }
